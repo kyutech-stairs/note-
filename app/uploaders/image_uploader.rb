a686bdbbd897b1614725ed3f8971e8abb19e0910
伊藤直人 <naoto67@itounaotonoMacBook-puro.local>
2018-06-14 14:40:34 +0900

    create show and image uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -20,28 +20,35 @@ class ImageUploader < CarrierWave::Uploader::Base
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
-
-  # Process files as they are uploaded:
-  # process scale: [200, 300]
-  #
+  #limit_size 1000x1000
+  process resize_to_limit: [1000,1000]
+  #保存形式
+  process convert: 'jpg'
+  
   # def scale(width, height)
   #   # do something
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  version :thumb do
+    process resize_to_fit: [50, 50]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+# 拡張子が同じでないとGIFをJPGとかにコンバートできないので、ファイル名を変更
+  def filename
+    super.chomp(File.extname(super)) + '.jpg' if original_filename.present?
+  end
 
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+ # ファイル名は日本語が入ってくると嫌なので、下記のようにしてみてもいい。
+ # 日付(20131001.jpgみたいなファイル名)で保存する
+  def filename
+    time = Time.now
+    name = time.strftime('%Y%m%d%H%M%S') + '.jpg'
+    name.downcase
+  end
 end
