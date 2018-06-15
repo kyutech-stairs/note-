73e969be34d28ef7e5718d5572f139f7372f56f3
伊藤直人 <naoto67@itounaotonoMacBook-puro.local>
2018-06-16 01:29:52 +0900

    add article form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_path unless current_user?(@user)
+  end
+  def current_user?(user)
+    current_user == user
+  end
+  
+  def qiita_markdown(markdown)
+    processor = Qiita::Markdown::Processor.new
+    processor.call(markdown)[:output].to_s.html_safe
+  end
 end
