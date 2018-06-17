1dd21789c582fb6fcec50ec8f4baca9d37748898
伊藤直人 <naoto67@itounaotonoMacBook-puro.local>
2018-06-18 07:17:17 +0900

    add users yaml file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :correct_user, only: [:edit, :update]
+  before_action :authenticate_user!, except: :show
 
   def show
     @user = User.find(params[:id])
