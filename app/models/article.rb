f6b193df7b47c3a3dc87ef4d05f3b486fa356969
野見山陸 <riku@nomiyamarikunoMacBook-puro.local>
2018-06-27 00:30:17 +0900

    set article price mos

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :prices, dependent: :destroy
+  accepts_nested_attributes_for :prices
   validates :title, presence: true
   validates :content, presence: true
 end
