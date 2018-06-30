7f810df0d846e22688cbd684d07328fa90050137
伊藤直人 <naoto67@itounaotonoMacBook-puro.local>
2018-06-30 14:48:48 +0900

    delete rename migrate file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180627062345) do
+ActiveRecord::Schema.define(version: 20180614014512) do
 
   create_table "articles", force: :cascade do |t|
     t.text "title"
