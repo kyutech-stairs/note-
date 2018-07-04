1a2f8385b89bde337eae69e848c098b79f9fb720
伊藤直人 <naoto67@itounaotonoMacBook-puro.local>
2018-07-04 23:12:32 +0900

    adjust users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @articles = @user.articles.paginate(page: params[:page], per_page: 10).order("created_at DESC")
+    @articles = @user.articles.paginate(page: params[:page], per_page: 18).order("created_at DESC")
   end
 
   def edit
