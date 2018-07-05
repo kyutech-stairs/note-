b4b2312cb981a76bbe4a27e60de99e5ade7c354b
伊藤直人 <naoto67@itounaotonoMacBook-puro.local>
2018-07-06 01:18:45 +0900

    add favorite articles in users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 18).order("created_at DESC")
+    @favorites = Article.where(id: @user.likes.map(&:article_id)).paginate(page: params[:page], per_page: 18).order("created_at DESC")
   end
 
   def edit
