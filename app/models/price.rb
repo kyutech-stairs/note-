f29574bd043aefc62e42cc6c11429b46893eac99
伊藤直人 <naoto67@itounaotonoMacBook-puro.local>
2018-07-07 01:14:26 +0900

    make change in price model and that associate view

--- app/models/price.rb
+++ app/models/price.rb
@@ -1,19 +1,23 @@
 class Price < ApplicationRecord
   belongs_to :article
-  enum min: { min_plan_0: 0, min_plan_1: 10, min_plan_2: 100, min_plan_3: 500, min_plan_4: 1000 }
-  enum max: { max_plan_0: 0, max_plan_1: 500, max_plan_2: 1000 ,max_plan_3: 2000, max_plan_4: 5000 }
-  enum rate: { free: 0, high: 1, normal: 2, low: 3 }
-
-  validates :min, presence: true
-  validates :max, presence: true
-  validates :rate, presence: true
+  enum rate: { free: 0, low: 1, middle: 2, high: 3 }
   validate :price_validation
 
+  validates :min, presence: true, inclusion: { in: 0..1000}, exclusion: { in: 1..99 }
+  validates :max, presence: true, inclusion: { in: 0..5000}, exclusion: { in: 1..99 }
+  validates :rate, presence: true
+
   def price_validation
-    if min_before_type_cast.to_i > max_before_type_cast.to_i
-      errors.add(:min, "min price should be smaller than max price.")
-    elsif min_before_type_cast.to_i ==0 && max_before_type_cast.to_i != 0 || rate_before_type_cast.to_i != 0
-      errors.add(:max, "if min is 0, max and rate should be 0.")
+    if min.nil?
+      errors.add(:min, " : 開始価格を入力してください")
+    elsif max.nil?
+      errors.add(:max, " : 最大価格を入力してください")
+    elsif rate.nil?
+      errors.add(:rate, " : レートを入力してください")
+    elsif min>max 
+      errors.add(:min, " : 開始価格を最高価格より小さく設定してください")
+    elsif min==0 && (max!=0 || rate != "free")
+      errors.add(:min, " : 開始価格が0の時最高価格・レートを0に設定してください")
     end
   end
 
--- test/models/price_test.rb
+++ test/models/price_test.rb
@@ -25,4 +25,26 @@ class PriceTest < ActiveSupport::TestCase
     @price.rate = nil
     assert_not @price.valid?
   end
+  test "min range should be inclusion correct range" do
+    @price.min = 1
+    assert_not @price.valid?
+    @price.min = 1001
+    assert_not @price.valid?
+  end
+  test "max range should be inclusion correct range" do
+    @price.max = 1
+    assert_not @price.valid?
+    @price.max=5001
+    assert_not @price.valid?
+  end
+  test "price validation method should be correctly" do
+    @price.min = 1000
+    @price.max = 100
+    assert_not @price.valid?
+    @price.min = 0
+    assert_not @price.valid?
+    @price.max = 0
+    @price.rate = "low"
+    assert_not @price.valid?
+  end 
 end
