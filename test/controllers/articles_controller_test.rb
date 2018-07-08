8291986a2e513c86c08b54c3652b20ddff442ffd
伊藤直人 <naoto67@itounaotonoMacBook-puro.local>
2018-07-09 03:08:03 +0900

    add article controller test

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -40,6 +40,83 @@ class ArticleControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_path
   end
 
+  #create
+  test "should create" do
+    log_in_as @user
+    assert_difference 'Article.count', 1 do
+      assert_difference 'Price.count', 1 do
+        post articles_path, params: 
+          {article: { title: "title", content: "content",
+                      price_attributes: { min: 0, max: 0, rate: "free" } } }
+      end
+    end
+  end
+  test "should redirect create when user are no logged in" do
+    assert_no_difference 'Article.count' do
+      assert_no_difference 'Price.count' do
+        post articles_path, params: 
+          {article: { title: "title", content: "content",
+                      price_attributes: { min: 0, max: 0, rate: "free" } } }
+      end
+    end
+    assert_redirected_to new_user_session_path
+  end
+  test "should redirect create when wrong user done" do
+    log_in_as @other
+    assert_difference 'Article.count', 1 do
+      assert_difference 'Price.count', 1 do
+        post articles_path, params: 
+          {article: { title: "title", content: "content",
+                      price_attributes: { min: 0, max: 0, rate: "free" } } }
+      end
+    end
+  end
+
   #update
+  test "should redirect update when user are no logged in" do
+    patch article_path(@article), params: { article: { title: "change"}}
+    assert_redirected_to new_user_session_path
+  end
+  test "should update when correct user done" do
+    log_in_as @user
+    patch article_path(@article), params: {article: {title: "change", content: "change"}}
+    @article.reload
+    assert @article.title == "change"
+    assert @article.content == "change"
+  end
+  test "should redirect update when wrong user done" do
+    log_in_as @other
+    patch article_path(@article), params: {article: {title: "change", content: "change"}}
+    assert_redirected_to root_path
+  end 
+
+  #destroy
+  test "should destroy" do
+    log_in_as @user
+    assert_difference 'Article.count', -1 do
+      assert_difference 'Price.count', -1 do
+        delete article_path(@article)
+      end
+    end 
+    assert_redirected_to root_path
+  end
+  test "should redirect destroy when user are no logged in" do
+    assert_no_difference 'Article.count' do
+      assert_no_difference 'Price.count' do
+        delete article_path(@article)
+      end
+    end 
+    assert_redirected_to new_user_session_path
+  end
+  test "should redirect destroy when wrong user done" do
+    log_in_as @other
+    assert_no_difference 'Article.count' do
+      assert_no_difference 'Price.count' do
+        delete article_path(@article)
+      end
+    end 
+    assert_redirected_to root_path
+  end
+
   
 end
