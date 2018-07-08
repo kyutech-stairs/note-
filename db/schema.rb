cf6c0cee23505e7d9490d4d9bc0d7cf91c350f1a
伊藤直人 <naoto67@itounaotonoMacBook-puro.local>
2018-07-08 23:03:06 +0900

    add follow function

--- db/schema.rb
+++ db/schema.rb
@@ -37,13 +37,11 @@ ActiveRecord::Schema.define(version: 20180705112846) do
   end
 
   create_table "follows", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "follow_id"
+    t.integer "follower_id"
+    t.integer "following_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["follow_id"], name: "index_follows_on_follow_id"
-    t.index ["user_id", "follow_id"], name: "index_follows_on_user_id_and_follow_id", unique: true
-    t.index ["user_id"], name: "index_follows_on_user_id"
+    t.index ["follower_id", "following_id"], name: "index_follows_on_follower_id_and_following_id", unique: true
   end
 
   create_table "likes", force: :cascade do |t|
