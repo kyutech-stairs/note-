7c9db939f488229645970d4ccdbf0a0ff363a08b
伊藤直人 <naoto67@itounaotonoMacBook-puro.local>
2018-07-20 01:40:58 +0900

    add update and destroy action in review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :destroy, :index, :update]
+  before_action :correct_user, only: [:destroy, :update]
 
   def index
     @articles = Article.where(id: current_user.purchases.map(&:article_id))
@@ -10,21 +11,34 @@ class ReviewsController < ApplicationController
     @review.user = current_user
     @review.article = Article.find(params[:article_id])
     if @review.save
-      set_flash(:notice, "reviewを更新しました")
-      redirect_to action: :index
+      set_flash(:notice, "reviewを作成しました")
     else
-      set_flash(:alert, "")
+      set_flash(:alert, "失敗")
     end
+    redirect_to action: :index
   end
 
   def destroy
-
+     @review.destroy
+     redirect_to action: :index
   end
   
+  def update
+    if @review.update_attributes(review_params)
+      set_flash(:notice, "reviewを更新しました")
+    else 
+      set_flash(:alert, "失敗")
+    end
+    redirect_to action: :index
+  end
   
   private 
   def review_params
     params.permit(:star, :title, :content)
+  end
+  def correct_user
+    @review = Review.find_by(id: params[:id])
+    redirect_to root_path if !@review
   end
     
 end
