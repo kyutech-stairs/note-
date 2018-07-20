d350d210ecd0f9135c6574824f460de34103f55c
伊藤直人 <naoto67@itounaotonoMacBook-puro.local>
2018-07-20 20:18:33 +0900

    add star rating component for reviews

---
+++ app/javascript/packs/star.js
@@ -0,0 +1,10 @@
+import Vue from 'vue/dist/vue'
+import App from '../app.vue'
+import StarRating from 'vue-star-rating'
+
+var star = new Vue({
+  el: "#star-rate",
+  components: {
+    'star-rating': StarRating
+  }
+})
