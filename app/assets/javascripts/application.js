973b99cdc0ae9a9b1d2d634ca6b2f01dc8ad9269
野見山陸 <riku@nomiyamarikunoMacBook-puro.local>
2018-08-15 14:09:11 +0900

    fix error2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,18 +17,3 @@
 //= require marked
 //= require turbolinks
 //= require_tree .
-
-
-// $(document).ready(function(){
-//    $('.dropdown-trigger').dropdown();
-//    $('.modal').modal();
-//    $('select').formSelect();
-//    $('.select-dropdown').prop('disabled', true);
-//    $('input[type="checkbox"]').change(function() {
-//       if ($(this).prop('checked')) {
-//         $('.select-dropdown').prop('disabled', true);
-//       } else {
-//         $('.select-dropdown').prop('disabled', false);
-//       }
-//    });
-//  });
--- app/assets/javascripts/article.js
+++ app/assets/javascripts/article.js
@@ -1,17 +1,3 @@
-// $(document).ready(function(){
-//    $('.modal').modal();
-//    $('select').formSelect();
-//    $('.select-dropdown').prop('disabled', true);
-//    $('input[type="checkbox"]').change(function() {
-//       if ($(this).prop('checked')) {
-//         $('.select-dropdown').prop('disabled', true);
-//       } else {
-//         $('.select-dropdown').prop('disabled', false);
-//       }
-//    });
-//  });
-
-
 $(document).ready(function(){
    $('.dropdown-trigger').dropdown();
    $('.modal').modal();
