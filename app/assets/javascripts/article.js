12e8b67a4200adab6f2bca5f71c1b400b531eb7a
野見山陸 <riku@nomiyamarikunoMacBook-puro.local>
2018-08-15 10:49:31 +0900

    fix modal disabled

--- app/assets/javascripts/article.js
+++ app/assets/javascripts/article.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
    $('.modal').modal();
    $('select').formSelect();
+   $('.select-dropdown').prop('disabled', true);
+   $('input[type="checkbox"]').change(function() {
+      if ($(this).prop('checked')) {
+        $('.select-dropdown').prop('disabled', true);
+      } else {
+        $('.select-dropdown').prop('disabled', false);
+      }
+   });
  });
