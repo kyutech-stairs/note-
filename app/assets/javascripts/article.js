299222c7aee892e8ac891fe47ca11d245af23f05
野見山陸 <riku@nomiyamarikunoMacBook-puro.local>
2018-08-15 13:29:13 +0900

    fix error

--- app/assets/javascripts/article.js
+++ app/assets/javascripts/article.js
@@ -1,4 +1,19 @@
+// $(document).ready(function(){
+//    $('.modal').modal();
+//    $('select').formSelect();
+//    $('.select-dropdown').prop('disabled', true);
+//    $('input[type="checkbox"]').change(function() {
+//       if ($(this).prop('checked')) {
+//         $('.select-dropdown').prop('disabled', true);
+//       } else {
+//         $('.select-dropdown').prop('disabled', false);
+//       }
+//    });
+//  });
+
+
 $(document).ready(function(){
+   $('.dropdown-trigger').dropdown();
    $('.modal').modal();
    $('select').formSelect();
    $('.select-dropdown').prop('disabled', true);
