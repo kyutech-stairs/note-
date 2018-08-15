7456dc19a95ac114450b68469e5a86cdbed13bf6
野見山陸 <riku@nomiyamarikunoMacBook-puro.local>
2018-08-15 13:09:29 +0900

    fix read js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,14 +21,5 @@
 
 $(document).ready(function(){
    $('.dropdown-trigger').dropdown();
-   $('.modal').modal();
-   $('select').formSelect();
-   $('.select-dropdown').prop('disabled', true);
-   $('input[type="checkbox"]').change(function() {
-      if ($(this).prop('checked')) {
-        $('.select-dropdown').prop('disabled', true);
-      } else {
-        $('.select-dropdown').prop('disabled', false);
-      }
-   });
+   
  });
