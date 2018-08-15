4b9248f01de007c108c628fc205366091b919ef9
野見山陸 <riku@nomiyamarikunoMacBook-puro.local>
2018-08-15 13:04:14 +0900

    fix modal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,5 +21,14 @@
 
 $(document).ready(function(){
    $('.dropdown-trigger').dropdown();
-
+   $('.modal').modal();
+   $('select').formSelect();
+   $('.select-dropdown').prop('disabled', true);
+   $('input[type="checkbox"]').change(function() {
+      if ($(this).prop('checked')) {
+        $('.select-dropdown').prop('disabled', true);
+      } else {
+        $('.select-dropdown').prop('disabled', false);
+      }
+   });
  });
--- vendor/bundle/ruby/2.5.0/gems/bootstrap-glyphicons-0.0.1/lib/bootstrap/glyphicons/railtie.rb
+++
@@ -1,6 +0,0 @@
-module Bootstrap
-  module Glyphicons
-    class Railtie < ::Rails::Railtie
-    end
-  end
-end
