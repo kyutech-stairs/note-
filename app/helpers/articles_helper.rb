66755a71177e84c9d375834ac5999df538f00b0b
野見山陸 <riku@nomiyamarikunoMacBook-puro.local>
2018-08-16 12:36:04 +0900

    markdown help2

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -5,35 +5,61 @@ module ArticlesHelper
   end
 
   def tooltip_h
-    html = "<div style=&quot;border-left: 5px solid #dddddd; padding: 5px;&quot;>
-              <h3 style=&quot;border-bottom: 1px solid #9e9e9e;&quot;>見出しh3</h3>
+    html = "<p>Headers</p><br/>
+            「#」,「##」....<br/><br/>
+            <div style=&quot;border-left: 5px solid #dddddd; padding: 5px;&quot;>
+              <p>プレビュー</p><h3 style=&quot;border-bottom: 1px solid #9e9e9e;&quot;>見出しh3</h3>
               <p>###見出しh3</p>
-              <h2 style=&quot;border-bottom: 1px solid #9e9e9e;&quot;>見出しh2</h2>
-              <p>##見出しh2</p>
-              <h1 style=&quot;border-bottom: 1px solid #9e9e9e;&quot;>見出しh1</h1>
-              <p>#見出しh1</p>
             </div>"
     html.html_safe
   end
 
   def tooltip_code
-    html =  "<p>コードの挿入</p><br/>
+    html =  "<p>Code</p><br/>
             <div style=&quot;border-left: 5px solid #dddddd; padding: 5px;&quot;>
-            ```php<br/>
+            (空行)<br/>
+            ```ruby:qiita.rb<br/>
               function hello(){<br/>
               　return &quot;hello world!&quot;;<br/>
               }<br/>
-            ```</div>"
+            ```<br/>
+            (空行)</div>"
     html.html_safe
   end
 
   def tooltip_link
-    html = "<p>リンクの挿入</p><br/>
+    html = "<p>Links</p><br/>
             [リンクテキスト](URL)<br/><br/>
           <div style=&quot;border-left: 5px solid #dddddd; padding: 5px;&quot;>
-            <p style=&quot;border-bottom: 1px solid #9e9e9e; color: #65a931;&quot;>Trucle</p>
+              <p>プレビュー</p><p style=&quot;border-bottom: 1px solid #9e9e9e; color: #65a931;&quot;>Trucle</p>
             [Trucle](<span style=&quot;color: #65a931;&quot;>http://trucle.com/</span>)
           </div>"
-    html.html_safe      
+    html.html_safe
+  end
+
+  def tooltip_list
+    html =  "<p>Lists</p><br/>
+            「-」,「+」,「*」<br/><br/>
+            <div style=&quot;border-left: 5px solid #dddddd; padding: 5px;&quot;>
+            - リスト１<br/>
+            &nbsp;&nbsp;- ネスト　リスト1-1<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;- ネスト　リスト1-1-1<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;- ネスト　リスト1-1-2</div>"
+    html.html_safe
+  end
+
+  def tooltip_quote
+    html =  "<p>Quote</p><br/>
+            「>」,「>>」<br/><br/>
+            <p>プレビュー</p>
+            <div style=&quot;border-left: 5px solid #dddddd; padding: 5px;&quot;>
+            本日はよろしくお願いします。<br/><br/>
+            <div style=&quot;border-left: 5px solid #dddddd; padding: 5px;&quot;>
+            こちらこそよろしくお願いします。<br/>
+            </div></div>
+            <p style=&quot;border-bottom: 1px solid #9e9e9e;&quot;></p>
+            > 本日はよろしくお願いします。<br/><br/>
+            >> こちらこそよろしくお願いします。"
+    html.html_safe
   end
 end
