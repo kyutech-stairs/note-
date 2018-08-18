19afda90ce5172b1ce25fff7505ebd765733091c
野見山陸 <riku@nomiyamarikunoMacBook-puro.local>
2018-08-18 19:31:35 +0900

    fit article show layout3

--- app/assets/javascripts/article.js
+++ app/assets/javascripts/article.js
@@ -1,13 +1,37 @@
 $(document).ready(function(){
    $('.dropdown-trigger').dropdown();
+   //article new:floatbtn
    $('.fixed-action-btn').floatingActionButton();
+   //article new:help
    $('.tooltipped').tooltip();
+   //ariticle show:acordion
    $('.collapsible').collapsible({
      accordion: false
    });
+   //purchase/price modal
    $('.modal').modal();
+   //price modal
    $('select').formSelect();
    $('.select-dropdown').prop('disabled', true);
+
+
+   $(window).scroll(function() { //スクロールしたら処理を開始する
+        var scroll = $(window).scrollTop() + $(window).height();
+        var footer = $("footer").offset().top;
+        if (scroll > footer+300) {
+            $(".sidebar").css({
+                "position": "absolute",
+                "top": footer-100,
+            });
+        } else {
+            $(".sidebar").css({　
+                "position": "fixed",
+                "top": "",
+            });
+        }
+    });
+
+  //price modalのselectbox
    $('input[type="checkbox"]').change(function() {
       if ($(this).prop('checked')) {
         $('.select-dropdown').prop('disabled', true);
