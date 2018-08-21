42afa565061662f170cecc8fa3c41cf590faed2c
伊藤直人 <naoto67@itounaotonoMacBook-puro.local>
2018-08-21 16:28:25 +0900

    fix search action in static pages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,7 +8,7 @@ class StaticPagesController < ApplicationController
   def search
     @users = User.search(params[:keyword])
     @users = @users.page(params[:user]).per(12) if @users
-    @articles = Article.search(params[:keyword]).order("created_at DESC")
-    @articles = @articles.page(params[:article]).per(24) if @articles
+    @articles = Article.search(params[:keyword])
+    @articles = @articles.order("created_at DESC").page(params[:article]).per(24) if @articles
   end
 end
