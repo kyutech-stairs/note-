024687584f835ee5a86de0b920093096e59f9a65
野見山陸 <riku@nomiyamarikunoMacBook-puro.local>
2018-09-24 16:10:00 +0900

    adjust markdown

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -4,6 +4,24 @@ module ArticlesHelper
     price.to_s + " 円"
   end
 
+  def tooltip_line_break
+    html = "<div style=&quot; border-bottom: 1px solid; &quot;>
+              <p>Line Breaks</p>
+            </div><br/>
+            <label>Text</label>
+            <div style=&quot; padding: 5px;&quot;>
+              <p>１行目（スペース２ついれる）</p>
+              <p>２行目</p>
+            </div>
+            <label>Preview</label>
+            <div style=&quot; padding: 5px;&quot;>
+              <p>１行目</p>
+              </br>
+              <p>２行目</p>
+            </div>"
+    html.html_safe
+  end
+
   def tooltip_h
     html = "<div style=&quot; border-bottom: 1px solid; &quot;>
               <p>Headers</p>
@@ -14,7 +32,7 @@ module ArticlesHelper
             </div>
             <label>Preview</label>
             <div style=&quot; padding: 5px;&quot;>
-              <h5;>Header5</h5>
+              <h5>Header5</h5>
             </div>"
     html.html_safe
   end
@@ -31,7 +49,7 @@ module ArticlesHelper
               }<br/>
             ```</div>
             <label>Preview</label>
-            <div style=&quot; padding: 5px; background-color: #394548; color: #e3e3e3; &quot;>
+            <div style=&quot; padding: 5px; background-color: #364548; color: #e3e3e3; &quot;>
               function hello(){<br/>
               　return &quot;hello world!&quot;;<br/>
               }<br/>
@@ -87,6 +105,23 @@ module ArticlesHelper
             <div style=&quot;border-left: 5px solid #dddddd; padding: 5px;&quot;>
             こちらこそよろしくお願いします。<br/>
             </div></div>"
+    html.html_safe
+  end
+
+  def tooltip_horizontal
+    html = "<div style=&quot; border-bottom: 1px solid; &quot;>
+              <p>Horizontal Lines</p>
+            </div><br/>
+            <label>Text</label>
+            <div style=&quot; padding: 5px;&quot;>
+              <p>***　　　　　　　　　</p>
+            </div>
+            <label>Preview</label>
+            <div style=&quot; padding: 5px;&quot;>
+              </br>
+              <div style=&quot; border-bottom: 1px solid; margin-right: 20px; &quot;></div>
+              </br>
+            </div>"
     html.html_safe
   end
 
